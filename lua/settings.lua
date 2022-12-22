93e5fdb60abc0663f9c90739169809a31c82e77d
Atharva Patil <atharva.patil@berkeley.edu>
2022-12-21 21:37:04 -0800

    Reordered settings + enabled cursorline.

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,15 +1,16 @@
 vim.opt.termguicolors = true
-vim.g.mapleader = ' '
-vim.wo.number = true
-vim.o.background = 'dark'
 vim.opt.wrap = false
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 vim.opt.errorbells = false
-vim.cmd('set mouse=')
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.o.background = 'dark'
+vim.wo.number = true
+vim.wo.cursorline = true
+vim.g.mapleader = ' '
+vim.cmd('set mouse=')
 
 vim.cmd[[colorscheme tokyonight-storm]]
