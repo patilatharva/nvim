f2081e51de8591322bf9ea86858911852488af07
Atharva Patil <atharva.patil@berkeley.edu>
2022-12-24 12:59:03 +0900

    Set up toggleterm.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -290,5 +290,35 @@ return require('packer').startup(function(use)
             })
         end
     }
+
+    -- toggleterm
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
+    require("toggleterm").setup{
+        size = function(term)
+            if term.direction == "horizontal" then
+                return 15
+            elseif term.direction == "vertical" then
+                return vim.o.columns * 0.3
+            end
+        end,
+        open_mapping = [[<c-\>]],
+        direction = 'vertical',
+    }
+
+    function _G.set_terminal_keymaps()
+        local opts = {buffer = 0}
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    end
+
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 end)
 
